74a429cfed23f54af9b655eaa9b64c5dfd881a5f
woodwindfreak101 <43051946+woodwindfreak101@users.noreply.github.com>
2018-10-09 15:22:04 -0500

    Finish web-caesar

---
+++ main.py
@@ -0,0 +1,37 @@
+from flask import flask
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+form = """
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <style>
+            form {
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
+                font: 16px sans-serif;
+                border-radius: 10px;
+            }
+            textarea {
+                margin: 10px 0;
+                width: 540px;
+                height: 120px;
+            }
+        </style>
+    </head>
+    <body>
+        <FORM HERE>
+    </body>
+</html>
+"""
+
+@app.route("/")
+def index():
+    return "Hello World"
+
+app.run()
